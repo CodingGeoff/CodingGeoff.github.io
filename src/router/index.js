b9aa012ad0d18c90cb82044eebd3bfedbb9f9fcd
CodingGeoff <boc_lc@126.com>
2024-07-22 22:58:17 +0800

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -29,7 +29,7 @@ const routes = [
     name: "DesktopWithPost",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"臻的天地"
     }
   },
   {
@@ -37,7 +37,7 @@ const routes = [
     name: "Desktop",
     component: Desktop,
     meta : {
-      title:"Welcom to WEN's blog"
+      title:"臻的天地"
     }
   },
   {
